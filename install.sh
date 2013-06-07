01723cc9a3cb8388653a62be552b37c7788b6bb9
Steven La <mrstevenla@gmail.com>
2013-06-07 15:30:05 -0700

    not sure if install script works or not

--- install.sh
+++ install.sh
@@ -1,7 +1,22 @@
-#! /bin/sh
-git submodule init &&
-git submodule update &&
-rm -f ~/.vimrc &&
-ln -s ~/.vim/vimrc ~/.vimrc &&
-echo "Successfully installed vimrc" ||
+#! /bin/bash
+
+$PWD=$(pwd)
+
+echo "Initializing git submodules..." &&
+( git submodule init && git submodule update ) &&
+
+echo "Linking vim config..." &&
+( rm -f ~/.vimrc && ln -s $PWD/vim/vimrc ~/.vimrc ) &&
+( rm -f ~/.vim && ln -s $PWD/vim ~/.vim ) &&
+
+echo "Linking zsh config..." &&
+( rm -f ~/.zshrc && ln -s $PWD/zsh/zshrc ~/.zshrc ) &&
+
+echo "Linking tmux config..." &&
+( rm -f ~/.tmux.conf && ln -s $PWD/tmux/tmux.conf ~/.tmux.conf) &&
+
+echo "Linking powerline..." &&
+( rm -f ~/.config/powerline && ln -s $PWD/powerline ~/.config/powerline) &&
+
+echo "Successfully installed dotfiles!" ||
 echo "Install failed"
