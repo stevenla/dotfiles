330ade4bc8a30303f12849e42e48aa7dd293c12e
Steven La <mrstevenla@gmail.com>
2013-08-01 01:32:09 -0700

    zsh vundle shortcut, cloen in install

--- install.sh
+++ install.sh
@@ -15,8 +15,9 @@ echo "Linked zsh config!" &&
 ( rm -f ~/.tmux.conf && ln -s $PWD/tmux/tmux.conf ~/.tmux.conf) &&
 echo "Linked tmux config!" &&
 
+( git clone https://github.com/gmarik/vundle.git vim/bundle/vundle ) &&
 ( vim +BundleInstall +qall ) &&
-echo "Installed Vundles!" &&
+echo "Installed Vundle!" &&
 
 echo "Successfully installed dotfiles!" ||
 echo "Install failed"
