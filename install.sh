68bceebc4d705c07e8e876d8f04b2d53a9644d41
Steven La <mrstevenla@gmail.com>
2017-01-11 15:27:41 -0800

    fish setup

--- install.sh
+++ install.sh
@@ -2,15 +2,17 @@
 
 PWD=$(pwd)
 
-( git clone https://github.com/robbyrussell/oh-my-zsh.git $PWD/zsh/oh-my-zsh) &&
-( git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $PWD/zsh/custom/plugins/zsh-syntax-highlighting) &&
+( curl -Lo ~/.config/fish/functions/fisher.fish --create-dirs git.io/fisher ) &&
+( rm -f ~/.config/fish/config.fish && ln -s $PWD/fish/config.fish ~/.config/fish/config.fish ) &&
+( rm -f ~/.config/fish/fishfile && ln -s $PWD/fish/fishfile ~/.config/fish/fishfile ) &&
+echo "Installed fisherman!" &&
+
+( rm -f ~/.zshrc && ln -s $PWD/zsh/zshrc ~/.zshrc ) &&
+echo "Linked zsh config!" &&
 
 ( rm -f ~/.vimrc && ln -s $PWD/vim/vimrc ~/.vimrc ) &&
 ( rm -rf ~/.vim && ln -s $PWD/vim ~/.vim ) &&
 echo "Linked vim config!" &&
-
-( rm -f ~/.zshrc && ln -s $PWD/zsh/zshrc ~/.zshrc ) &&
-echo "Linked zsh config!" &&
 
 ( rm -f ~/.tmux.conf && ln -s $PWD/tmux/tmux.conf ~/.tmux.conf) &&
 echo "Linked tmux config!" &&
